71a88b19fceab75534eb5ed87a491d33d802e473
Cristian Somesan <crs@flink.to>
2020-04-07 15:03:36 +0300

    Add relation between columns and tasks

--- app/models/column.rb
+++ app/models/column.rb
@@ -2,6 +2,7 @@ class Column < ApplicationRecord
   # Model associations
   belongs_to :board
   belongs_to :created_by, class_name: "User", foreign_key: "created_by_id"
+  has_many :tasks
 
   # Validations
   validates_presence_of :created_by, :name, :board
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
+  # Model associations
   belongs_to :created_by, class_name: "User", foreign_key: "created_by_id"
-
   # a ticket can be unassigned
   belongs_to :assigned_to, class_name: "User", foreign_key: "assigned_to_id", optional: true
+  belongs_to :column
 
-  validates_presence_of :created_by, :title, :order
+  # Validations
+  validates_presence_of :created_by, :title, :order, :column
 end
