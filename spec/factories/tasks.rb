6ed041435f2e9bf05a5d61e9f812d7424712a179
Cristian Somesan <crs@flink.to>
2020-04-07 14:45:15 +0300

    Add rake task to verify if factories are valid

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,14 @@
+# FactoryBot.define do
+#   factory :user do
+#     title { Faker::Lorem.sentence }
+#     created_by { create(:user) }
+#     order { 'Password4&' }
+#   end
+# end
+
+
+#     t.string "name", null: false
+#     t.text "description"
+#     t.bigint "created_by_id", null: false
+#     t.bigint "assigned_to_id"
+#     t.integer "order", null: failsafe
