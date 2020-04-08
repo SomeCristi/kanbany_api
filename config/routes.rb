6c3a07f2070b1dc0ff487ade500b5d57357a8891
Cristian Somesan <crs@flink.to>
2020-04-08 18:34:37 +0300

    Add routes for columns resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :boards
+  resources :boards do
+    resources :columns
+    # post :add_user TODO add a way to add users to board
+  end
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
