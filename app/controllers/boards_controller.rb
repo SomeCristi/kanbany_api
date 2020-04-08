ce3e5020dd99826351d74cff304fe42d94ea325b
Cristian Somesan <crs@flink.to>
2020-04-08 15:11:40 +0300

    Add boards#update boards#show and boards#index + tests

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,16 +1,44 @@
 class BoardsController < ApplicationController
-  before_action :set_board, if: :has_membership?, only: [:show, :update, :destroy]
+  before_action :set_board, only: [:show, :update, :destroy]
 
   # POST /boards
+  # creates a new board record with the requested params
+  # returns 201 is successful
+  # returns 422 if the provided params are not good
   def create
     board = Board.create!(board_params.merge(created_by: @current_user))
     json_response(board, :created)
   end
 
-  # PUT /boards
+  # PUT/PATCH /boards/:id
+  # updates the board with the requested params
+  # returns 200 is successful
+  # returns 422 if the provided params are not good
+  # returns 404 if the resource does not exist
+  # return 403 if user is not a member
+  # returns 401 if request is unauthorized
   def update
     @board.update!(board_params)
     json_response(@board)
+  end
+
+  # GET /boards/:id
+  # returns the requested board
+  # returns 200 if successful
+  # returns 404 if resource does not exists
+  # return 403 if user is not a member
+  # returns 401 if request is unauthorized
+  def show
+    json_response(@board)
+  end
+
+  # GET /boards
+  # returns the boards of which the user is a member
+  # returns 200 if successful
+  # returns 401 if request is unauthorized
+  def index
+    @boards = Board.joins(:memberships).where('memberships.user_id= ?', @current_user.id)
+    json_response(@boards)
   end
 
   private
@@ -21,8 +49,10 @@ class BoardsController < ApplicationController
 
   def set_board
     @board = Board.find(params[:id])
+    json_response({ message: "The requested resource does not exist" }, :forbidden) unless has_membership?
   end
 
+  # checks is the current user is a member of the requested board
   def has_membership?
     Membership.is_member?(@current_user.id, params[:id])
   end
--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -6,10 +6,17 @@ RSpec.describe 'Boards API', type: :request do
   let(:valid_params) do
     { board: attributes_for(:board) }
   end
+  let(:board_name) { Faker::Lorem.word }
+  # the board is created by the user because he will be added
+  # as a member automatically
+  # let! is used insead of let because in the GET boards test the
+  # objects were not persisted to the DB
+  # explanation here: https://relishapp.com/rspec/rspec-core/v/3-5/docs/helper-methods/let-and-let
+  let!(:board) { create(:board, created_by: user, name: board_name) }
+  let!(:not_a_member_board) { create(:board) }
 
   # Board creation test suite
   describe 'POST /boards' do
-
     context 'when valid request' do
       subject { post '/boards', params: valid_params, headers: headers }
 
@@ -66,15 +73,18 @@ RSpec.describe 'Boards API', type: :request do
         expect(json["message"]).to include("Validation failed: Name can't be blank")
       end
     end
+
+    context 'when unauthorized request' do
+      before { post "/boards", params: valid_params, headers: missing_token_headers }
+
+      it 'returns HTTP status 401' do
+        expect(response).to have_http_status(401)
+      end
+    end
   end
 
   # Board update test suite
   describe 'PUT /boards/:id' do
-    let(:board_name) { Faker::Lorem.word }
-    # the board is created by the user because he will be added
-    # as a member automatically
-    let(:board) { create(:board, created_by: user, name: board_name) }
-
     context 'when valid request' do
       before { put "/boards/#{board.id}", params: valid_params, headers: headers }
 
@@ -112,7 +122,99 @@ RSpec.describe 'Boards API', type: :request do
       before { put "/boards/#{board.id}", params: valid_params, headers: missing_token_headers }
 
       it 'returns HTTP status 401' do
-        # binding.pry
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context 'when user is not a member' do
+      before { put "/boards/#{not_a_member_board.id}", params: valid_params, headers: headers }
+
+      it 'returns HTTP status 403' do
+        expect(response).to have_http_status(403)
+      end
+    end
+  end
+
+  describe 'GET /boards/:id' do
+    context 'when valid request' do
+      before { get "/boards/#{board.id}", headers: headers }
+
+
+      it 'returns HTTP status 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the correct name' do
+        expect(json["name"]).to eq(board.name)
+      end
+    end
+
+    context 'when board does not exist' do
+      before { get "/boards/#{board.id + 100}", headers: headers }
+
+      it 'returns HTTP status 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context 'when user is not a member' do
+      before { get "/boards/#{not_a_member_board.id}", headers: headers }
+
+      it 'returns HTTP status 403' do
+        expect(response).to have_http_status(403)
+      end
+    end
+
+    context 'when unauthorized request' do
+      before { get "/boards/#{board.id}", headers: missing_token_headers }
+
+      it 'returns HTTP status 401' do
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
+
+  describe 'GET /boards' do
+    let!(:second_board) { create(:board, created_by: user) }
+
+    context 'when valid request' do
+      before { get "/boards", headers: headers }
+
+      it 'returns HTTP status 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all the objects' do
+        expect(json.size).to eq(2)
+      end
+
+      it 'does not include the boards that the user is not a member of' do
+        ids = json.map { |entry| entry['id']}
+        expect(ids).not_to include(not_a_member_board.id)
+      end
+
+      it 'returns all the correct ids' do
+        objects_id = [board.id, second_board.id].sort
+        response_ids = json.map { |entry| entry['id']}.sort
+        expect(response_ids).to eq(objects_id)
+      end
+
+      it 'returns all the correct names' do
+        objects_name = [board.name, second_board.name].sort
+        response_names = json.map { |entry| entry['name']}.sort
+        expect(response_names).to eq(objects_name)
+      end
+
+      it 'returns all the correct created_by_ids' do
+        created_by_ids = [board.created_by_id, second_board.created_by_id].sort
+        expect( json.map { |entry| entry['created_by_id']}.sort).to eq(created_by_ids)
+      end
+    end
+
+    context 'when unauthorized request' do
+      before { get "/boards", headers: missing_token_headers }
+
+      it 'returns HTTP status 401' do
         expect(response).to have_http_status(401)
       end
     end
