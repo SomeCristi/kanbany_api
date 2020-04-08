b94e59405e35d108f391e10189ab8e7ddf517aa6
Cristian Somesan <crs@flink.to>
2020-04-08 18:30:51 +0300

    Refactor commentaries

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,11 +1,12 @@
 class BoardsController < ApplicationController
-  before_action :set_board, only: [:show, :update, :destroy]
+  before_action :set_board, only: [:show, :update]
 
   # POST /boards
   # creates a new board record with the requested params
-  # returns 201 is successful
+  # created_by_id attribute takes the current user id
+  # returns 201 if successful
   # returns 422 if the provided params are not good
-  # returns 401 if request is unauthorized and an error message
+  # returns 401 and an error message if request is unauthorized
   def create
     board = Board.create!(board_params.merge(created_by: @current_user))
     json_response(board, :created)
@@ -13,11 +14,11 @@ class BoardsController < ApplicationController
 
   # PUT/PATCH /boards/:id
   # updates the board with the requested params
-  # returns 200 is successful
+  # returns 200 if successful
   # returns 422 if the provided params are not good
   # returns 404 if the resource does not exist
   # return 403 if user is not a member
-  # returns 401 if request is unauthorized and an error message
+  # returns 401 and an error message if request is unauthorized
   def update
     @board.update!(board_params)
     json_response(@board)
@@ -28,7 +29,7 @@ class BoardsController < ApplicationController
   # returns 200 if successful
   # returns 404 if resource does not exists
   # return 403 if user is not a member
-  # returns 401 if request is unauthorized and an error message
+  # returns 401 and an error message if request is unauthorized
   def show
     json_response(@board)
   end
@@ -36,7 +37,7 @@ class BoardsController < ApplicationController
   # GET /boards
   # returns the boards of which the user is a member
   # returns 200 if successful
-  # returns 401 if request is unauthorized and an error message
+  # returns 401 and an error message if request is unauthorized
   def index
     @boards = Board.joins(:memberships).where('memberships.user_id= ?', @current_user.id)
     json_response(@boards)
@@ -48,12 +49,7 @@ class BoardsController < ApplicationController
     params.require(:board).permit(:name)
   end
 
-  def set_board
-    @board = Board.find(params[:id])
-    json_response({ message: "The requested resource does not exist" }, :forbidden) unless has_membership?
-  end
-
-  # checks is the current user is a member of the requested board
+  # checks if the current user is a member of the requested board
   def has_membership?
     Membership.is_member?(@current_user.id, params[:id])
   end
