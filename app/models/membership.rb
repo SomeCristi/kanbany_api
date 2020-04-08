ed241a4c7ed542849f1619bf688dbd101f129a37
Cristian Somesan <crs@flink.to>
2020-04-08 13:24:23 +0300

    Add functions to check is user is a board members and to add members

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,12 @@ class Membership < ApplicationRecord
 
   # Validations
   validates_presence_of :user, :board
+
+  def self.is_member?(user_id, board_id)
+    self.where(user_id: user_id, board_id: board_id).exists?
+  end
+
+  def self.add_membership(user_id, board_id)
+    self.create(user_id: user_id, board_id: board_id)
+  end
 end
