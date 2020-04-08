35f70a84a83919db412a9b362b2e4941e3eb38a7
Cristian Somesan <crs@flink.to>
2020-04-08 12:13:37 +0300

    Modify memberships migration

--- db/migrate/20200408090341_create_memberships.rb
+++ db/migrate/20200408090341_create_memberships.rb
@@ -1,8 +1,8 @@
 class CreateMemberships < ActiveRecord::Migration[6.0]
   def change
     create_table :memberships do |t|
-      t.belongs_to :board_id, null: false
-      t.belongs_to :user_id, null: false
+      t.belongs_to :board, null: false
+      t.belongs_to :user, null: false
 
       t.timestamps
     end
