06410577a0f069b0bb11186add4009522d2c0008
Cristian Somesan <crs@flink.to>
2020-04-08 18:30:39 +0300

    Add methods to get the board and column from parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,23 @@ class ApplicationController < ActionController::API
   # called before every action on controllers
   before_action :authorize_request
 
+  def set_board
+    id = params[:board_id].present? ? params[:board_id] : params[:id]
+    @board = Board.find(id)
+    json_response({ message: "Forbidden from accessing this board" }, :forbidden) unless has_membership?
+  end
+
+  def set_column
+    id = params[:column_id].present? ? params[:column_id] : params[:id]
+    @column = Column.find(id)
+  end
+
   private
+
+  # checks if the current user is a member of the requested column
+  def has_membership?
+    Membership.is_member?(@current_user.id, params[:id])
+  end
 
   # Check for valid request token and return user
   def authorize_request
