1410b6d67b1681bc1fa2fc455c906a8a52ce7da6
Cristian Somesan <crs@flink.to>
2020-04-09 22:28:47 +0300

    Add change role route

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+  put '/users/:id/change_role', to: 'users#change_role'
 end
